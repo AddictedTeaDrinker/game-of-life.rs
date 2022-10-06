1644849063d975aec097d56321d33e43d31d5570
AddictedTeaDrinker <addicted.tea.drinker@gmail.com>
2022-10-06 21:04:55 +0200

    Implement game of life logic

---
+++ src/board.rs
@@ -0,0 +1,88 @@
+#[derive(Clone, Copy, PartialEq)]
+pub enum State {
+    Dead,
+    Alive,
+}
+
+pub struct Board {
+    width: i32,
+    height: i32,
+    cells: Vec<State>,
+}
+
+impl Board {
+    pub fn new(width: i32, height: i32) -> Self {
+        Board {
+            width: width,
+            height: height,
+            cells: vec![State::Dead; (width * height) as usize],
+        }
+    }
+
+    pub fn get_cell(&self, x: i32, y: i32) -> State {
+        if x >= self.width || y >= self.height || x < 0 || y < 0 {
+            State::Dead
+        } else {
+            self.cells[(y * self.width + x) as usize]
+        }
+    }
+
+    pub fn set_cell(&mut self, x: i32, y: i32, state: State) {
+        self.cells[(y * self.width + x) as usize] = state;
+    }
+
+    pub fn print_board(&self) {
+        for j in 0..self.height {
+            for i in 0..self.width {
+                if self.get_cell(i, j) == State::Dead {
+                    print!(".");
+                } else {
+                    print!("#");
+                }
+            }
+            println!("");
+        }
+    }
+
+    pub fn count_neighbors(&self, x: i32, y: i32) -> i32 {
+        let mut count = 0;
+        for i in x - 1..=x + 1 {
+            for j in y - 1..=y + 1 {
+                if self.get_cell(i, j) == State::Alive {
+                    count = count + 1;
+                }
+            }
+        }
+        if self.get_cell(x, y) == State::Alive {
+            count = count - 1;
+        }
+        count
+    }
+
+    pub fn next_generation(&self) -> Board {
+        let mut board = Board::new(self.width, self.height);
+        for j in 0..self.height {
+            for i in 0..self.width {
+                let alive_neighbours = self.count_neighbors(i, j);
+                if alive_neighbours == 3 && self.get_cell(i, j) == State::Dead {
+                    board.set_cell(i, j, State::Alive);
+                } else if (alive_neighbours == 2 || alive_neighbours == 3)
+                    && self.get_cell(i, j) == State::Alive
+                {
+                    board.set_cell(i, j, State::Alive);
+                } else {
+                    board.set_cell(i, j, State::Dead);
+                }
+            }
+        }
+        board
+    }
+}
+
+// OOO
+// O.O
+// OOO
+
+// - martwa komórka, która ma dokładnie 3 żywych sąsiadów, staje się żywa, inaczej zostaje martwa
+// - żywa komórka z 2 lub 3 żywymi sąsiadami zostaje żywa, inaczej umiera
+// count_neighbors(x, y) -> liczba żywych sąsiadów
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
+mod board;
 mod canvas;
 
+use board::{Board, State};
 use canvas::{Canvas, Color};
 
 fn main() {
-    let mut canvas = Canvas::new(800, 600);
-    canvas.rect(150, 100, 300, 500, Color::Black);
-    canvas.print_pbm();
+    let mut board = Board::new(10, 10);
+    board.set_cell(1, 0, State::Alive);
+    board.set_cell(2, 1, State::Alive);
+    board.set_cell(0, 2, State::Alive);
+    board.set_cell(1, 2, State::Alive);
+    board.set_cell(2, 2, State::Alive);
+    board.print_board();
+
+    loop {
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        board = board.next_generation();
+        println!("");
+        board.print_board();
+    }
 }
